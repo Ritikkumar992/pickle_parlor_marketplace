b570871330b07e27118055f5da7be9ae435b27c1
Ritik <ritik151112@gmail.com>
2023-01-11 01:00:26 +0530

    firstCommit

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> Wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91-9775301685 </p>
+         <p> <i class="fas fa-phone"></i> +91-9693882391 </p>
+         <!-- <a href="https://wa.me/9775301685?text=Hello" style="font-size:1.5rem;" target="_blank">+91-7004025874</a> -->
+         <p> <i class="fas fa-envelope"></i> ritik151112@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> kolkata, india - 400104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>Ritik Kumar</span> | all rights reserved! </p>
+
+</footer>
